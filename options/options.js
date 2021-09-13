c20af7bf92d0c4c87fa52c7aa90f2b581e710be6
Crowdbotics <team@crowdbotics.com>
2021-09-13 12:03:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "misty_moon_30498Identifier",
+  url: "https://misty_moon_30498Identifier.botics.co",
+  api: "https://misty_moon_30498Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
